0c0c478ed4375de1635e159830b6ec542e33dceb
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2025-02-28 22:29:45 +0530

    Time: 0 ms (100%), Space: 10 MB (57.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-    /*bool isPalindrome(string s,int i=0) {
-       int n=s.size();
-        if(i>n/2) return true;
-        if(s[i]!=s[n-i-1]) return false;
-        return isPalindrome(s,i+1);
-        
-    }*/
     bool isPalindrome(string s){
         int l=0,r=s.size()-1;
         while(l<r){
