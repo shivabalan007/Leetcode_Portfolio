a53b09fbeb213bef222cd7fe85695db32ec192a5
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2025-07-08 23:44:49 +0530

    Time: 24 ms (7.69%), Space: 12.5 MB (57.83%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def relativeSortArray(self, arr1, arr2):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :rtype: List[int]
+        """
+        res = []
+
+        for i in range(len(arr2)):
+            for j in range(len(arr1)):
+                if(arr1[j]==arr2[i]):
+                    res.append(arr1[j])
+                    arr1[j]=-1
+        arr1.sort()
+
+        for num in arr1:
+            if num != -1:
+                res.append(num)
+        return res
