63c9997d46ea6c189566feb918506ff3bf17995e
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2025-08-27 22:58:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.49%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def alternateDigitSum(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        pos = 0
+        neg = 0
+        digi = str(n)
+        for i in range(len(digi)):
+            if(i%2 == 0):
+                pos+=int(digi[i])
+            else:
+                neg+=int(digi[i])
+        
+        return pos-neg
+            
+        
