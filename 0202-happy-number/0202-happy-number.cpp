ec64bb8b92e74c858648f80edd6c3501949a3b38
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2025-09-11 07:19:19 +0530

    Time: 4 ms (6.85%), Space: 8.3 MB (56.51%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,17 +1,23 @@
 class Solution {
+private:
+    int newNum(int n){
+        int sum = 0;
+        while(n!=0){
+            int num = n%10;
+            sum+= num*num;
+            n = n/10;
+        }
+        return sum;
+    }
+     
 public:
     bool isHappy(int n) {
-        int temp = n;
-        while(temp != 0){
-            int last = temp%10;
-            temp = temp/10;
-            int total = last*last + temp*temp;
+       unordered_set<int> set;
+       while(n !=1 && !set.count(n)){
+        set.insert(n);
+        n = newNum(n);
+       }
 
-            temp = total;
-            if(total==1) return true;
-            
-        }
-
-        return false;
+       return n==1;
     }
 };
