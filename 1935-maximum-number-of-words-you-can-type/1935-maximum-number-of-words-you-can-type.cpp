a7545803793d7ea6ff005eb229e2fe2d48562af2
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2025-09-15 09:52:48 +0530

    Time: 0 ms (100%), Space: 8.8 MB (79.4%) - LeetHub

---
+++ 1935-maximum-number-of-words-you-can-type/1935-maximum-number-of-words-you-can-type.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int canBeTypedWords(string text, string brokenLetters) {
+        vector<int> ch(26,0);
+
+        for(int i=0;i<brokenLetters.size();i++){
+            ch[brokenLetters[i]-'a'] = 1;
+        }
+         
+        int cnt=0,ans=0;
+        for(int i=0;i<text.length();i++){
+            if(text[i]==' '){
+                if(cnt==0) ans++;
+                cnt=0;
+            }else if(ch[text[i]-'a']==1){
+                cnt++;
+            }
+
+        }
+
+        if(cnt==0) ans++;
+
+        return ans;
+        
+    }
+};
