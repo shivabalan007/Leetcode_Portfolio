cbd5413a1ca42ab435d5c83258354f85eb8c2c0f
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2026-06-10 22:16:57 +0530

    Time:3 ms (53.13%), Space:20.5 MB (42.44%) - Easy

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        result = []
-        for i, n in enumerate(nums):
-            for j in range(i+1,len(nums)):
-                if n+nums[j] == target:
-                    result.extend([i,j])
-                    return result
-                
+        hash_key = {}
+        for i in range(len(nums)):
+            y = target - nums[i]
+            if y in hash_key:
+                return [hash_key[y],i]
+
+            hash_key[nums[i]] = i
+
