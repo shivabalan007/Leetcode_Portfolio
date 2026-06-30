75461ad1645f2724153812024d56f7236c42c363
shivabalan007 <106253640+shivabalan007@users.noreply.github.com>
2026-06-30 21:50:10 +0530

    Time:0 ms (100%), Space:19.2 MB (76.41%) - Easy

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,28 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        arr = []
-        
-        n1 = 0
-        n2 = 0
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
 
-        while n1 < m and n2 < n:
-            if nums1[n1] < nums2[n2]:
-                arr.append(nums1[n1])
-                n1 += 1
+        while(j>=0):
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                arr.append(nums2[n2])
-                n2 += 1
-            
-        while n1 < m:
-            arr.append(nums1[n1])
-            n1 += 1
-        
-        while n2 < n:
-            arr.append(nums2[n2])
-            n2 += 1
-        
-        for i in range(len(arr)):
-            nums1[i] = arr[i]
-        
-            
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+
+
+# TC - O(n+m)
+# SC - O(1)
